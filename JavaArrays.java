047c2aa56ef29dd5c4f981b0cd24f2eb499154cb
elinabaktygulova <ellabaktygulova@gmail.com>
2025-01-23 19:40:02 +0600

    Update JavaArrays.java

--- JavaArrays.java
+++ JavaArrays.java
@@ -5,7 +5,7 @@ public class JavaArrays {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Ввод массива
+
         System.out.println("Введите количество элементов массива :");
         int n = scanner.nextInt();
 
@@ -20,18 +20,16 @@ public class JavaArrays {
             array[i] = scanner.nextInt();
         }
 
-        // Сумма элементов
         int sum = 0;
         for (int num : array) {
             sum += num;
         }
         System.out.println("Сумма элементов: " + sum);
 
-        // Среднее значение
         double average = (double) sum / n;
         System.out.println("Среднее значение: " + average);
 
-        // Минимальный и максимальный элемент
+
         int min = array[0];
         int max = array[0];
 
@@ -48,10 +46,8 @@ public class JavaArrays {
         System.out.println("Максимальный элемент: " + max);
 
 
-        // Сортировка массива
         Arrays.sort(array);
 
-        // Второй минимальный и второй максимальный элемент
         System.out.println("Второй минимальный элемент: " + array[1]);
         System.out.println("Второй максимальный элемент: " + array[n - 2]);
 
